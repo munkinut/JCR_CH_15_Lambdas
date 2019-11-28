71e4dfaf844fe419292de79fb80405b7c55d5c76
Warren Milburn <warren@munki.net>
2019-11-28 13:08:09 +0000

    Changed to factorial demo.

--- src/net/munki/play/lambdas/BlockLambdaDemo.java
+++ src/net/munki/play/lambdas/BlockLambdaDemo.java
@@ -1,22 +1,21 @@
 package net.munki.play.lambdas;
 
-interface StringFunc {
-	String func(String n);
+interface NumericFunc {
+	int func(int n);
 }
 
 public class BlockLambdaDemo {
 
 	public static void main(String[] args) {
         // TODO Auto-generated method stub
-        StringFunc reverse = (str) -> {
-            String result = "";
-            int i;
-            for (i = str.length()-1; i >= 0; i--)
-                result += str.charAt(i);
+        NumericFunc factorial = (n) -> {
+            int result = 1;
+            for (int i = 1; i <= n; i++)
+                result = i * result;
             return result;
-        };
-        System.out.println("Lambda reversed is " + reverse.func("Lambda"));
-        System.out.println("Expression reversed is " + reverse.func("Expression"));
+        };            
+        System.out.println("The factorial of 3 is " + factorial.func(3));
+        System.out.println("The factorial of 5 is " + factorial.func(5));
 
 	}
 
